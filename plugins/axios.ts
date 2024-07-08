7d8620cd55ce9891d446289c6e59f80b59df2f1e
Taha Uçar <taha.ucar@tardigrad.com>
2024-07-08 09:56:17 +0300

    toast duration updated

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -11,16 +11,16 @@ export default defineNuxtPlugin((nuxtApp) => {
         (error) => {
             if (error.response.status === 401) {
                 const message = error.response.status + ' ' + error.response.data;
-                $toast(message, 3000, toastClass);
+                $toast(message, 4000, toastClass);
             } else if (error.response.status === 404) {
                 const message = error.response.status + ' ' + error.response.data;
-                $toast(message, 3000, toastClass);
+                $toast(message, 4000, toastClass);
             } else if (error.response.status === 422) {
                 const message = error.response.status + ' ' + error.response.data;
-                $toast(message, 3000, toastClass);
+                $toast(message, 4000, toastClass);
             } else if (error.response.status === 500) {
                 const message = error.response.status + ' ' + error.response.data;
-                $toast(message, 3000, toastClass);
+                $toast(message, 4000, toastClass);
             }
         }
     );
